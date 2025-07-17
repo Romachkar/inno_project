51ec1e92731f60908ace558378f141b482062172
Maiky_n <kbobrov773@gmail.com>
2025-07-17 09:37:41 +0300

    update bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from handlers import register_handlers
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
+
+
+register_handlers(dp)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(dp.start_polling(bot))
